8055315d1d1f326469991f7e4c18aa5896160b4c
PandaTea23 <145078669+PandaTea23@users.noreply.github.com>
2025-08-25 13:17:45 -0400

    Update recruiting.js
    Final updates to dates

--- app/data/recruiting.js
+++ app/data/recruiting.js
@@ -1,10 +1,10 @@
 export const APPLY_URL = 'https://forms.gle/MMw2gpNFQDsFGfTZ9';
 export const EVENTS = [
-  { title: 'Festifall', start: '2025-08-27T09:00:00', end: '2025-08-27T09:30:00', location: 'Session 2 A30', description: 'Come get to know the club! ' },
+  { title: 'Festifall', start: '2025-08-27T16:30:00', end: '2025-08-27T18:00:00', location: 'Session 2 A30', description: 'Come get to know the club! ' },
   { title: 'Mass Meeting 1', start: '2025-08-28T18:00:00', end: '2025-08-28T19:00:00', location: 'League Henderson', description: 'Get to know the club!' },
-  { title: 'Mass Meeting 2', start: '2025-09-01T19:00:00', end: '2025-08-30T20:00:00', location: 'Union Anderson', description: 'Get to know the club moareeee!!!' },
-  { title: 'Resume Workshop', start: '2025-09-02T19:00:00', end: '2025-08-30T20:00:00', location: 'Mason Hall 3437', description: 'Come by to learn how you can improve your resume, not just for when applying to clubs but als0 for jobs!' },
-  { title: 'Inclusivity Panel', start: '2025-09-05T19:00:00', end: '2025-09-07T20:00:00', location: 'Mason Hall 3437', description: 'Come by to see how MDC handles inclusion as a whole' },
-  { title: 'Applications Close ', start: '2025-09-09T23:59:59', description: 'This is a hard deadline, no exceptions will be made' },
-  { title: 'Release Decision', start: '2025-09-15T10:00:00', location: 'Check your emails', description: 'Gogogooooo' },
+  { title: 'Mass Meeting 2', start: '2025-09-01T19:00:00', end: '2025-08-01T20:00:00', location: 'Union Anderson', description: 'Get to know the club moareeee!!!' },
+  { title: 'Resume Workshop', start: '2025-09-03T19:00:00', end: '2025-08-03T20:00:00', location: 'Mason Hall 3437', description: 'Come by to learn how you can improve your resume, not just for when applying to clubs but als0 for jobs!' },
+  { title: 'Inclusivity Panel', start: '2025-09-05T19:00:00', end: '2025-09-05T20:00:00', location: 'Mason Hall 3437', description: 'Come by to see how MDC handles inclusion as a whole' },
+  { title: 'Applications Close ', start: '2025-09-05T23:59:59', description: 'This is a hard deadline, no exceptions will be made' },
+  { title: 'Release Decision', start: '2025-09-07T10:00:00', location: 'Check your emails', description: 'Gogogooooo' },
 ];
