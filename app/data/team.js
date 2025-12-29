4672fec7c63d8db29a735aa50e83c7e12a18e345
Brighton Han <hanbrigh@umich.edu>
2025-12-28 20:24:13 -0500

    Slightly changed wording

--- app/data/team.js
+++ app/data/team.js
@@ -4,122 +4,258 @@ export const MEMBERS = [
     title: 'Executive Board',
     members: [
       {
-        name: 'Vincent Wu',
-        title: 'President',
-        grade: 'Junior',
-        image: '/members/vincent_wu.jpg',
-        links: {
-          email: 'bigpanda@umich.edu',
-          linkedin: 'https://linkedin.com/in/vinwu1',
-          github: 'https://github.com/vinwu1',   
-        },
-      },
-      {
         name: 'Emma Peng',
-        title: 'VP, Recruitment',
+        title: 'President',
         grade: 'Junior',
         image: '/members/emma_peng.jpg',
         links: {
           email: 'emmapeng@umich.edu',
-          linkedin: 'https://www.linkedin.com/in/emma-peng28/',
+          linkedin: 'https://www.linkedin.com/in/emma-peng28/'
         },
       },
       {
-        name: 'Aidan Olman',
-        title: 'VP, Internal',
-        grade: 'Junior',
-        image: '/members/aidan_olman.jpg',
+        name: 'Kyle Gwon',
+        title: 'VP, Recruitment',
+        grade: 'Sophomore',
+        image: '/members/kyle_gwon.jpg',
         links: {
-          email: 'aolman@umich.edu',
-          linkedin: 'https://www.linkedin.com/in/aolman/',
+          email: 'kylegwon@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/kyle-gwon/',
+        },
+      },
+      {
+        name: 'Harsimran Kaur Singh',
+        title: 'VP, Internal',
+        grade: 'Sophomore',
+        image: '/members/harsimran_kaur_singh.jpg',
+        links: {
+          email: 'hksingh@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/harsimrankaursingh/',
+        },
+      },
+      {
+        name: 'Rithvik Vinod',
+        title: 'VP, External Relations',
+        grade: 'Junior',
+        image: '/members/rikki_vinod.jpg',
+        links: {
+          email: 'Rithvin@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/rithvikvinod/',
         },
       },
       {
         name: 'Cyrus Desai',
-        title: 'VP, External Relations',
+        title: 'VP, Technical Development',
         grade: 'Junior',
         image: '/members/cyrus_desai.jpg',
         links: {
-          email: 'cyrusd@umich.edu',
+          email: 'cdesai@umich.edu',
           linkedin: 'https://www.linkedin.com/in/cyrus-desai/',
         },
       },
       {
-        name: 'Alex Zhang',
-        title: 'VP, Technical Development',
+        name: 'Alexander Zhang',
+        title: 'VP, Professional Development',
         grade: 'Junior',
-        //image: '/members/alex_zhang.jpg',
+        image: '/members/alex_zhang.jpg',
         links: {
           email: 'alexyz@umich.edu',
           linkedin: 'https://www.linkedin.com/in/alexyz1/',
         },
       },
       {
-        name: 'Pari Dar',
-        title: 'VP, Professional Development',
-        grade: 'Senior',
-        image: '/members/pari_dar.jpg',
-        links: {
-          email: 'paridar@umich.edu',
-          linkedin: 'https://www.linkedin.com/in/pdar/',
-        },
-      },
-      {
-        name: 'Rohan Athreya',
+        name: 'Brighton Han',
         title: 'VP, Finance',
-        grade: 'Junior',
-        //image: '/members/rohan_athreya.jpg',
+        grade: 'Freshman',
+        image: '/members/brighton_han.jpg',
         links: {
-          email: 'rathreya@umich.edu',
-          linkedin: 'https://www.linkedin.com/in/rohanmathreya/',
+          email: 'hanbrigh@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/brighton-han/',
         },
       },
       {
-        name: 'Anna Khoury',
+        name: 'Varshini Anbu',
         title: 'Chair, Socials',
         grade: 'Junior',
-        image: '/members/anna_khoury.jpg',
+        image: '/members/varshini_anbu.jpg',
         links: {
-          email: 'annakhou@umich.edu',
-          linkedin: 'https://www.linkedin.com/in/anna-khoury-332763303/',
+          email: 'vvanbu@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/vijayavarshini-anbu/',
         },
       },
       {
-        name: 'Evan Fitzjohn',
+        name: 'Srikarthik	Vangavolu',
         title: 'Chair, Inclusion',
         grade: 'Junior',
-        image: '/members/evan_fitzjohn.jpg',
+        //image: '/members/<name>.jpg',
         links: {
-          email: 'fitzjohn@umich.edu',
-          linkedin: 'https://www.linkedin.com/in/evan-fitzjohn/',
+          email: 'Karthkv@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/srikarthik-vangavolu-a16476280/',
+        },
+      },
+      {
+        name: 'Emma Zhang',
+        title: 'Chair, Conduct',
+        grade: 'Junior',
+        image: '/members/emma_zhang.jpg',
+        links: {
+          email: 'emmzhang@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/emmzhang/',
         },
       },
     ],
   },
+
+  {
+    key: 'senioradvisors',
+    title: 'Senior Advisors',
+    members: [
+      {
+        name: 'Rohan Athreya',
+        title: 'Senior Advisor',
+        grade: 'Senior',
+        image: '/members/rohan_athreya.jpg',
+        links: {
+          email: 'rathreya@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/rohanmathreya/'
+        },
+      },
+      {
+        name: 'Pari Dar',
+        title: 'Senior Advisor',
+        grade: 'Senior',
+        image: '/members/pari_dar.jpg',
+        links: {
+          email: 'paridar@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/pdar/'
+        },
+      },
+      {
+        name: 'Evan Fitzjohn',
+        title: 'Senior Advisor',
+        grade: 'Junior',
+        image: '/members/evan_fitzjohn.jpg',
+        links: {
+          email: 'fitzjohn@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/evan-fitzjohn/'
+        },
+      },
+      {
+        name: 'Anna Khoury',
+        title: 'Senior Advisor',
+        grade: 'Junior',
+        image: '/members/anna_khoury.jpg',
+        links: {
+          email: 'annakhou@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/anna-khoury-/'
+        },
+      },
+      {
+        name: 'Aidan Olman',
+        title: 'Senior Advisor',
+        grade: 'Junior',
+        image: '/members/aidan_olman.jpg',
+        links: {
+          email: 'aolman@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/aolman/'
+        },
+      },
+      {
+        name: 'Vincent Wu',
+        title: 'Senior Advisor',
+        grade: 'Junior',
+        image: '/members/vincent_wu.jpg',
+        links: {
+          email: 'bigpanda@umich.edu',
+          linkedin: 'https://www.linkedin.com/in/vinwu1/'
+        },
+      },
+    ],
+  },
+
   {
     key: 'analysts',
     title: 'Analysts',
     members: [
-      { name: 'David Ren',    title: 'Analyst', grade: 'Junior', image: '/members/david_ren.jpg',  links: { linkedin: 'https://www.linkedin.com/in/davidyren/' } },
-      { name: 'David Watkins',    title: 'Analyst', grade: 'Junior', image: '/members/david_watkins.jpg', links: { linkedin: 'https://www.linkedin.com/in/davidwatkins5/' } },
-      { name: 'Ellis Balser',    title: 'Analyst', grade: 'Junior', image: '/members/ellis_balser.jpg', links: { linkedin: 'https://www.linkedin.com/in/ellis-balser/' } },
-      { name: 'Emma Zhang',    title: 'Analyst', grade: 'Junior', image: '/members/emma_zhang.jpg',  links: { linkedin: 'https://www.linkedin.com/in/emmzhang/' } },
-      { name: 'Ethan Peterson',    title: 'Analyst', grade: 'Sophomore', /*image: '/members/ethan_peterson.jpg',*/ links: { linkedin: 'https://www.linkedin.com/in/ethan-peterson-6767732aa/' } },
-      { name: 'Henry Meinertzhagen',    title: 'Analyst', grade: 'Sophomore', image: '/members/henry_meinertzhagen.jpg', links: { linkedin: 'https://www.linkedin.com/in/henry-meinertzhagen-aa0014315/' } },
-      { name: 'Karla Vega',    title: 'Analyst', grade: 'Senior', image: '/members/karla_vega.jpg', links: { linkedin: 'https://www.linkedin.com/in/karla-vega-1a5494310/' } },
-      { name: 'Kevin Chen',    title: 'Analyst', grade: 'Junior', /*image: '/members/kevin_chen.jpg',*/ links: { linkedin: 'https://www.linkedin.com/in/kevin-chen-90066930b/' } },
-      { name: 'Krish Venkatachalam',    title: 'Analyst', grade: 'Junior', image: '/members/krish_venkatachalam.jpg', links: { linkedin: 'https://www.linkedin.com/in/krishanth-venkatachalam/' } },
-      { name: 'Kyle Gwon',    title: 'Analyst', grade: 'Sophomore', /*image: '/members/kyle_gwon.jpg',*/ links: { linkedin: 'https://www.linkedin.com/in/kyle-gwon/' } },
-      { name: 'Mathew Zheng',    title: 'Analyst', grade: 'Junior', /*image: '/members/mathew_zheng.jpg',*/ links: { linkedin: 'https://www.linkedin.com/in/mathew-zheng/' } },
-      { name: 'Michael Li',    title: 'Analyst', grade: 'Sophomore', image: '/members/michael_li.jpg', links: { linkedin: 'https://www.linkedin.com/in/lxymli/' } },
-      { name: 'Nikita Benny',    title: 'Analyst', grade: 'Sophomore', image: '/members/nikita_benny.jpg', links: { linkedin: 'https://www.linkedin.com/in/nikita-benny/' } },
-      { name: 'Marco Li',    title: 'Analyst', grade: 'Senior', image: '/members/marco_li.jpg', links: { linkedin: 'https://www.linkedin.com/in/qinkai-marco-li-88841026a/' } },
-      { name: 'Rikki Vinod',    title: 'Analyst', grade: 'Junior', image: '/members/rikki_vinod.jpg', links: { linkedin: 'https://www.linkedin.com/in/rithvikvinod/', github: 'https://github.com/Rithvin05' } },
-      { name: 'Karthik Vangavolu',    title: 'Analyst', grade: 'Junior', /*image: '/members/karthik_vangavolu.jpg',*/ links: { linkedin: 'https://www.linkedin.com/in/srikarthik-vangavolu-a16476280/' } },
-      { name: 'Stanley Czabanski',    title: 'Analyst', grade: 'Junior', image: '/members/stanley_czabanski.jpg', links: { linkedin: 'https://www.linkedin.com/in/stanley-czabanski/' } },
-      { name: 'Varshini Anbu',    title: 'Analyst', grade: 'Junior', image: '/members/varshini_anbu.jpg', links: { linkedin: 'https://www.linkedin.com/in/vijayavarshini-anbu/' } },
-      { name: 'Yufeng Lin',    title: 'Analyst', grade: 'Senior', /*image: '/members/yufeng_lin.jpg',*/ links: { linkedin: 'https://www.linkedin.com/in/yufenglin2026/' } },
+      { name: 'Ellis Balser', title: 'Analyst', grade: 'Junior', image: '/members/ellis_balser.jpg',
+        links: { email: 'ebalser@umich.edu', linkedin: 'https://www.linkedin.com/in/ellis-balser/' } },
+
+      { name: 'Nikita Benny', title: 'Analyst', grade: 'Sophomore', image: '/members/nikita_benny.jpg',
+        links: { email: 'nbenny@umich.edu', linkedin: 'https://www.linkedin.com/in/nikita-benny/' } },
+
+      { name: 'Charlene Chan', title: 'Analyst', grade: 'Freshman', image: '/members/charlene_chan.jpg',
+        links: { email: 'charlito@umich.edu', linkedin: 'https://www.linkedin.com/in/charlene-chan-610480238/' } },
+
+      { name: 'Kevin Chen', title: 'Analyst', grade: 'Junior',
+        links: { email: 'kchens@umich.edu', linkedin: 'https://www.linkedin.com/in/kevin-chen-90066930b/' } },
+
+      { name: 'Jonathan Chen', title: 'Analyst', grade: 'Freshman', image: '/members/jonathan_chen.jpg',
+        links: { email: 'jchenn@umich.edu', linkedin: 'https://www.linkedin.com/in/jonathanchen5/' } },
+
+      { name: 'Stanley Czabanski', title: 'Analyst', grade: 'Junior', image: '/members/stanley_czabanski.jpg',
+        links: { email: 'sczaba@umich.edu', linkedin: 'https://www.linkedin.com/in/stanley-czabanski/' } },
+
+      { name: 'Lucy Gong', title: 'Analyst', grade: 'Junior', image: '/members/lucy_gong.jpg',
+        links: { email: 'lucygong@umich.edu', linkedin: 'https://www.linkedin.com/in/lucy-q-gong/' } },
+
+      { name: 'Sean Gretzinger', title: 'Analyst', grade: 'Sophomore', image: '/members/sean_gretzinger.jpg',
+        links: { email: 'scgretz@umich.edu', linkedin: 'https://www.linkedin.com/in/sean-gretzinger/' } },
+
+      { name: 'Elan Hong', title: 'Analyst', grade: 'Sophomore',
+        links: { email: 'elanhong@umich.edu', linkedin: 'https://www.linkedin.com/in/elan-hong/' } },
+
+      { name: 'Joshua Kim', title: 'Analyst', grade: 'Sophomore',
+        links: { email: 'jjkk@umich.edu', linkedin: 'https://www.linkedin.com/in/joshuakim28/' } },
+
+      { name: 'Marco Li', title: 'Analyst', grade: 'Senior', image: '/members/marco_li.jpg',
+        links: { email: 'mliqk@umich.edu', linkedin: 'https://www.linkedin.com/in/qinkai-marco-li-88841026a/' } },
+
+      { name: 'Michael Li', title: 'Analyst', grade: 'Sophomore', image: '/members/michael_li.jpg',
+        links: { email: 'lxymli@umich.edu', linkedin: 'https://www.linkedin.com/in/lxymli/' } },
+
+      { name: 'Henry Meinertzhagen', title: 'Analyst', grade: 'Sophomore', image: '/members/henry_meinertzhagen.jpg',
+        links: { email: 'hmeinert@umich.edu', linkedin: 'https://www.linkedin.com/in/henry-meinertzhagen-aa0014315/' } },
+
+      { name: 'Evan Meranchik', title: 'Analyst', grade: 'Sophomore',
+        links: { email: 'emeranch@umich.edu', linkedin: 'https://www.linkedin.com/in/evan-meranchik/' } },
+
+      { name: 'Nathanael Mitchell', title: 'Analyst', grade: 'Freshman', image: '/members/nathanael_mitchell.jpg',
+        links: { email: 'natmitch@umich.edu', linkedin: 'https://www.linkedin.com/in/nathanael-mitchell-17b716347/' } },
+
+      { name: 'Eric Ni', title: 'Analyst', grade: 'Freshman', image: '/members/eric_ni.jpg',
+        links: { email: 'nier@umich.edu', linkedin: 'https://www.linkedin.com/in/erichni/' } },
+
+      { name: 'Lilly Penner', title: 'Analyst', grade: 'Junior',
+        links: { email: 'lpen@umich.edu', linkedin: 'https://www.linkedin.com/in/l-penner/' } },
+
+      { name: 'Sophia Pessoa', title: 'Analyst', grade: 'Junior', image: '/members/sophia_pessoa.jpg',
+        links: { email: 'sapessoa@umich.edu', linkedin: 'https://www.linkedin.com/in/sophiapessoa/' } },
+
+      { name: 'Ethan Peterson', title: 'Analyst', grade: 'Sophomore',
+        links: { email: 'ethpet@umich.edu', linkedin: 'https://www.linkedin.com/in/ethan-robert-peterson/' } },
+
+      { name: 'Adhit Ranjan', title: 'Analyst', grade: 'Freshman',
+        links: { email: 'adhit@umich.edu', linkedin: 'https://www.linkedin.com/in/adhit-ranjan-a7193a32a/' } },
+
+      { name: 'David Ren', title: 'Analyst', grade: 'Junior', image: '/members/david_ren.jpg',
+        links: { email: 'davidren@umich.edu', linkedin: '' } },
+
+      { name: 'Karla Vega', title: 'Analyst', grade: 'Senior', image: '/members/karla_vega.jpg',
+        links: { email: 'kvega@umich.edu', linkedin: 'https://www.linkedin.com/in/davidyren/' } },
+
+      { name: 'Krishanth Venkatachalam', title: 'Analyst', grade: 'Junior',
+        links: { email: 'krishven@umich.edu', linkedin: 'https://www.linkedin.com/in/krishanth-venkatachalam/' } },
+
+      { name: 'David Watkins', title: 'Analyst', grade: 'Junior', image: '/members/david_watkins.jpg',
+        links: { email: 'dwatkit@umich.edu', linkedin: 'https://www.linkedin.com/in/davidwatkins5/' } },
+
+      { name: 'Edward Zhang', title: 'Analyst', grade: 'Freshman', image: '/members/edward_zhang.jpg',
+        links: { email: 'edwardyz@umich.edu', linkedin: 'https://www.linkedin.com/in/edwardyz/' } },
+
+      { name: 'Mathew Zheng', title: 'Analyst', grade: 'Junior',
+        links: { email: 'mathewz@umich.edu', linkedin: 'https://www.linkedin.com/in/mathew-zheng/' } }
     ],
+
   },
   // {
   //   key: 'advisors',
