bc67dfcadd07a4a2c611b3b608cd24f3f6d8e959
Emma Peng <emmapeng@umich.edu>
2026-01-06 22:50:30 -0500

    recruitment wording

--- app/data/recruiting.js
+++ app/data/recruiting.js
@@ -2,11 +2,11 @@
 export const INTEREST_URL = "https://forms.gle/BJFLTZqUUuxG9MU69" // Previous: 'https://forms.gle/MMw2gpNFQDsFGfTZ9';
 export const APPLY_URL = "" // Previous: "https://forms.gle/uFq1PdZ8fhL3jFX27";
 export const EVENTS = [
-  { title: 'Winterfest', start: '2026-01-12T16:00:00', end: '2025-01-12T19:00:00', location: 'Michigan Union Rogel 6', description: 'Come get to know the club! ' },
+  { title: 'Winterfest', start: '2026-01-12T16:00:00', end: '2025-01-12T19:00:00', location: 'Michigan Union Rogel 6', description: 'Come get to know MDC! Talk to our members to see if our community is the right fit for you ' },
   { title: 'Mass Meeting 1', start: '2026-01-14T18:00:00', end: '2026-01-14T19:00:00', location: '', description: 'Meet some of the members and hear more about the club! Both mass meetings cover the same info, so we recommend just coming to one.' },
   { title: 'Mass Meeting 2', start: '2026-01-15T19:00:00', end: '2026-01-15T20:00:00', location: '', description: 'Meet some of the members and hear more about the club! Both mass meetings cover the same info, so we recommend just coming to one.' },
   { title: 'Resume Workshop', start: '2026-01-19T19:00:00', end: '2026-01-19T20:00:00', location: '', description: 'Come by to learn how you can improve your resume, not just for when applying to clubs but also for jobs!' },
-  { title: 'Inclusivity Panel', start: '2026-01-21T19:00:00', end: '2026-01-21T20:00:00', location: '', description: 'Come by to see how MDC handles inclusion as a whole.' },
-  { title: 'Applications Close ', start: '2026-01-22T23:59:59', description: 'This is a hard deadline, no exceptions will be made.' },
+  { title: 'Inclusivity Panel', start: '2026-01-21T19:00:00', end: '2026-01-21T20:00:00', location: '', description: 'Come by to see how MDC handles inclusion as a whole — this is a great opportunity to get to know our community and ask last minute application questions!' },
+  { title: '**Applications Close**', start: '2026-01-22T23:59:59', description: 'Please make sure to submit your application on time — no exceptions will be made for late submissions.' },
   { title: 'Release Decision', start: '2026-01-25T10:00:00', location: 'Check your emails', description: 'Good luck!' },
 ];
