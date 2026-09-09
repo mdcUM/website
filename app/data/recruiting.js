2fe9aee6e8756a13d451832bf754c9261c394e6c
Brighton Han <hanbrigh@umich.edu>
2026-09-09 14:00:52 -0400

    schedule update

--- app/data/recruiting.js
+++ app/data/recruiting.js
@@ -2,12 +2,8 @@
 export const INTEREST_URL = "https://forms.gle/VQr46G75E1YToWVU6" // Previous: 'https://forms.gle/MMw2gpNFQDsFGfTZ9';
 export const APPLY_URL = "https://docs.google.com/forms/d/e/1FAIpQLSd-kRdP7QaBnH05J-5ftcf5rXNhHWeaptGX5uUPQEm5MBEtBw/viewform?usp=dialog"
 export const EVENTS = [
-  { title: 'Festifall', start: '2026-09-02T18:00:00', end: '2026-09-02T20:00:00', location: 'E-064', description: 'Come get to know MDC! Talk to our members to see if our community is the right fit for you ' },
-  { title: 'Application Open', start: '2026-09-03T20:00:00', description: 'Applications for the Fall 2026 semester are now open!' },
-  { title: 'Mass Meeting 1', start: '2026-09-03T18:30:00', end: '2026-09-03T19:30:00', location: 'CCCB 3460', description: 'Meet our members and hear more in-depth about being an analyst in MDC!' },
-  { title: 'Mass Meeting 2', start: '2026-09-08T18:00:00', end: '2026-09-08T19:00:00', location: 'NUB 1552', description: 'Meet our members and hear more in-depth about being an analyst in MDC!' },
   { title: 'Resume Workshop', start: '2026-09-09T19:30:00', end: '2026-09-09T20:30:00', location: 'CCCB 3460', description: 'Understand what we look for in your resumes and hear from experienced members on optimizing your resume for both club and job applications' },
-  { title: 'DEI + Donuts', start: '2026-09-11T18:00:00', end: '2026-09-11T19:00:00', location: 'Room TBD', description: 'Learn about our values. Get to know MDC beyond the technical-side and how we build our community and team culture.' },
+  { title: 'DEI + Donuts', start: '2026-09-11T18:00:00', end: '2026-09-11T19:00:00', location: 'NUB 1552', description: 'Learn about our values. Get to know MDC beyond the technical-side and how we build our community and team culture.' },
   { title: '**Applications Close**', start: '2026-09-11T23:59:59', description: 'Please make sure to submit your application on time — no exceptions will be made for late submissions.' },
   { title: 'Case Workshop', start: '2026-09-13T16:00:00', end: '2026-09-13T17:00:00', location: 'Mason Hall 3330', description: 'Learn how to approach case interviews and see examples of past cases. Open to all students!' },
   { title: 'Tech Workshop', start: '2026-09-18T18:00:00', end: '2026-09-18T19:00:00', location: 'Mason Hall 3330', description: 'Our tech workshop is open to ALL students regardless of application status. Get a preview of how our education workshops build member skill development in the club.' },
